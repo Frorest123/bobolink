d98afc40f0037432d9a5d1b5457816f97abd2358
Frorest <fedecampa09@gmail.com>
2024-02-08 16:21:25 -0500

    removed unwanted period

--- app/math/Algebra2/page.js
+++ app/math/Algebra2/page.js
@@ -16,7 +16,7 @@ export default function Algebra2(){
             <p>+1 (954) 371 - 4062</p>
 
             <h2>Address</h2>
-            <p>.101 West 11th st <br></br>New York, NY  10011</p>
+            <p>101 West 11th st <br></br>New York, NY  10011</p>
         </div>
     </div>
 </section>
