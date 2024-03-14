98cb141860df3fa38d8f21eb89c03222d4f2f1b8
Frorest <fedecampa09@gmail.com>
2024-03-14 17:56:37 -0400

    youtube

--- app/footer.js
+++ app/footer.js
@@ -26,6 +26,7 @@ export default function () {
                         <li><Link href="">+1 (954) 371 - 4062</Link></li>
                         <li><Link href="https://twitter.com/Bobolink_AI">Twitter</Link></li>
                         <li><Link href="https://www.linkedin.com/company/bobolink-ai/">LinkedIn</Link></li>
+                        <li><Link href="https://www.youtube.com/@Bobolink-AI">Youtube</Link></li>
                     </ul>
                 </li>
             </ul>
