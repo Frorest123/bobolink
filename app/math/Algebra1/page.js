86cf725b89e4f5cf072be86a25253d0ec0e40f6b
Frorest <fedecampa09@gmail.com>
2024-03-14 17:21:24 -0400

    added images

--- app/math/Algebra1/page.js
+++ app/math/Algebra1/page.js
@@ -11,13 +11,13 @@ export default function Algebra1(){
             
             <div class="team-members">
                 <div class="team-member">
-                    <img src="https://bobolink.ai/ceo.jpg" alt="CEO" class="team-photo"></img>
+                    <img src="https://cdn.discordapp.com/attachments/1116071450659536977/1217945501769404416/e2abb86f-6184-4884-8ad5-43ad89ad5833.jpg?ex=6605df23&is=65f36a23&hm=b44fc3e6f1872573abc72289e119406e8953c443f5318b865ea25bd7bf340a01&" alt="CEO" class="team-photo"></img>
                     <h2>CEO</h2>
                     <p>Nicole Pochinki is our visionary seasoned leader with a passion for creating technologies that make a difference. She brings a rich blend of experience in educational technologies and pedagogic research. Her unique blend of innovation, empathy, and strategic vission guides Bobolink on its path to constantly pushing the boundaries of what is possible in EdTech.</p>
                 </div>
 
                 <div class="team-member">
-                    <img src="https://bobolink.ai/cto.jpg" alt="CTO" class="team-photo"></img>
+                    <img src="https://cdn.discordapp.com/attachments/1116071450659536977/1217945501505290330/372276a0-0fa5-45ea-aef5-2c17daac5e7d.jpg?ex=6605df23&is=65f36a23&hm=c361c5b5efea69917502563b055e86ce499c39f9dae1d90f8653d4209d9ad041&" alt="CTO" class="team-photo"></img>
                     <h2>CTO</h2>
                     <p>Joe Olson is the driving force behind our cutting-edge technology initiatives. With a PhD in Physics from Harvard University, his academic journey has spanned multiple disciplines, including computational neuroscience and educational data science. Curious by nature and focused by profession, he emodies the spirit of continuous learning that lies at the heart of our company's mission.</p>
                 </div>
