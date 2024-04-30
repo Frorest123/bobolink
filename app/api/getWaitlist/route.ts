e8c7197a8794f7d45fe8fdea3a0a068201d04294
Gustavo Falcon <gustavoestebanfalcon@gmail.com>
2024-04-30 08:21:35 -0400

    temporarily disabled getWaitlist route

--- app/api/getWaitlist/route.ts
+++ app/api/getWaitlist/route.ts
@@ -2,6 +2,6 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "@app/libs/prismadb"
 
 export async function GET() {
-    const waitlistMembers = await prisma.waitlistMember.findMany();
-    return NextResponse.json({waitlist: waitlistMembers}, {status: 200});
+    // const waitlistMembers = await prisma.waitlistMember.findMany();
+    return NextResponse.json({waitlist: [{id: 7, email: "test@testmail.jawn", leads: "wordOfMouth"}]}, {status: 200});
 }
